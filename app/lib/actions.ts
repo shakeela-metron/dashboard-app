83185f69b8caaae6196bd90efa123f187e2678fe
shakeelmetron <shakeel.ahmad@metronlabs.com>
2025-05-19 17:17:50 +0530

    implemented delete invoice records

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,6 +13,9 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
@@ -33,4 +36,28 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
